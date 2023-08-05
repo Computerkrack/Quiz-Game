9667968c39ba38d923786b43daf5bd5ecf348ce1
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-08-05 22:39:45 +0200

    added the ability to add questions

--- Quiz-Game.py
+++ Quiz-Game.py
@@ -1,12 +1,10 @@
-x = none
+x = None
 status = "setup"
-
 
 questions = []
 
 for i in range(10):
     questions.append(None)
-
 
 while status == "setup":
     try:
@@ -14,30 +12,6 @@ while status == "setup":
         status = "beginning"
     except ValueError:
         print("Bitte gib eine Zahl ein")
-
-
-# if playing == "2":
-#     status = "making"
-
-#     print("Hier siehts du eine Liste mit bereits gestellten Fragen")
-#     print("Frage1: " + )
-
-#     while status == "making":
-#         makingquestionnum = input('Welche Frage möchtest du füllen? (Falls du aufhören möchtest tippe quit ein) ')
-#         if makingquestionnum == "quit":
-#             status = "beginning"
-#         if makingquestionnum == "1":
-#             questiongood = x
-#             while questiongood != "ja":    # ist zwar schon kommentar aber egal kennst du booleans also true und false
-#                 Question = input("Was soll die Frage sein? ")
-#                 Question1 = Question
-#                 questiongood = input("Ist die Frage so gut?: " + Question + " ")
-
-#             answer_good = x
-#             while answergood != "ja":
-#                 answer = input("Gebe jetzt bitte die Anwort ein: ")
-#                 answer1 = answer 
-#                 answergood = input("Ist die Antwort so gut?: " + answer + " ")
         
 if status == "beginning":
     print("Wilkommen zum Quiz Spiel!")
@@ -60,12 +34,24 @@ match playing:
                     status = "exiting"
                 case _:
                     if making_question_num.isnumeric():
-                        pass # hier kann man dann checken das die zahl nicht zu groß oder zu klein ist, dafür einfach die var zu einem int machen und dann mit len() die länge der liste nehmen
-                        # wenn der index angemessen ist dann deine struktur zum schreiben musst halt auf das richtige listen element gehen
-                            # das wäre dann schon ein guter punkt könnte man einen git commit machen
-                            # naja ich gehe jetzt auch raus, schreibe mir dann einfach später nochmal
-                        # sonst dem user einfach eine fehlermeldung gönnen
+                        questions_len = len(questions)
+                        question_index = int(making_question_num) - 1
+
+                        if question_index <= 0:
+                            print('Bitte gib eine Zahl größer als 0 ein')
+                            continue
+                        elif question_index + 1 > questions_len:
+                            print(f'Die Frage {question_index + 1} existiert nicht!')
+                            continue
+                        else:
+                            question_text = input('Wie soll die Frage lauten? ')
+                            question_answer = input('Wie lautet die Antwort? ')
+
+                            questions[question_index] = {
+                                'question': question_text,
+                                'answer': question_answer
+                            }
+                        pass
                     else:
                         print("Bitte gebe eine Zahl ein!")
 
-# wenn man das nicht versteht dann gibt es das hier: ChatGPT
